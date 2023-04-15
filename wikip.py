cd740cd3aa7a334b4717a0cca54b0a2d993c65e1
nelson199608 <130792780+nelson199608@users.noreply.github.com>
2023-04-15 12:22:33 -0400

    Update wikip.py

--- wikip.py
+++ wikip.py
@@ -1,8 +1,10 @@
-from simplebot import SimpleBot
-import wikipedia
+from urllib.parse import quote_plus
 
-# Inicializar el bot
-bot = SimpleBot()
+import simplebot
+from deltachat import Message
+from simplebot.bot import DeltaBot, Replies
+from simplebot_instantview import prepare_html, session  # noqa
+import wikipedia
 
 # Definir la función para buscar en Wikipedia
 def search_wikipedia(query):
@@ -13,16 +15,12 @@ def search_wikipedia(query):
      except:
          return "Lo siento, no pude encontrar información sobre eso en 
 Wikipedia."
-
-# Definir el comando /wiki
-@bot.command('wiki')
-def wiki_command(message):
-     # Obtener el término de búsqueda
+@simplebot.command()
+	def wiki(bot: DeltaBot, message: Message, replies: Replies)->None:
+	"""Search in Wikipedia"""
+		# Obtener el término de búsqueda
      query = message.text.split(' ', 1)[1]
      # Realizar la búsqueda en Wikipedia
      result = search_wikipedia(query)
      # Enviar el resultado al usuario
      bot.send_message(message.chat.id, result)
-
-# Ejecutar el bot
-bot.run()
